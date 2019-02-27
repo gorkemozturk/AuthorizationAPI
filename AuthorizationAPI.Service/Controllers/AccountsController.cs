c53723e9bdb563b7d2cf0ab2f5988da87a59af4c
Görkem Öztürk <gorkemozturk@hotmail.com>
2019-02-27 13:26:43 +0300

    AccountsController has been implemented.

---
+++ AuthorizationAPI.Service/Controllers/AccountsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthorizationAPI.Service.Controllers
+{
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountsController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Credentials credentials)
+        {
+            var user = new IdentityUser { UserName = credentials.Email, Email = credentials.Email };
+            var result = await userManager.CreateAsync(user, credentials.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            await signInManager.SignInAsync(user, isPersistent: false);
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Credentials credentials)
+        {
+            var result = await signInManager.PasswordSignInAsync(credentials.Email, credentials.Password, false, false);
+
+            if (!result.Succeeded)
+                return BadRequest();
+
+            var user = await userManager.FindByEmailAsync(credentials.Email);
+
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id)
+            };
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("This is the secret phrase."));
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            var jwt = new JwtSecurityToken(signingCredentials: signingCredentials, claims: claims);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+        }
+    }
+}
